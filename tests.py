b3e9d205d05d179fdd56217c7ac53ffa1ce3be59
Oveatrint <kbaiyewu@gmail.com>
2026-06-13 21:25:33 +0100

    modularized fake path in tests

--- tests.py
+++ tests.py
@@ -64,6 +64,14 @@ error_responses = {
 }
 
 
+@pytest.fixture
+def temp_markdown_file(tmp_path, monkeypatch):
+    """Changes Markdown directory from markdown_dir to tmp_path"""
+    monkeypatch.setattr("main.markdown_dir", tmp_path)
+
+    return tmp_path
+
+
 def test_check_endpoint_rejects_non_markdown_files():
     file_content = b""
     file_name = "idk.txt"
@@ -81,7 +89,7 @@ def test_check_endpoint_rejects_non_markdown_files():
 def test_check_endpoint_accepts_markdown_file(mocker):
     file_name = "README.md"
     content_type = "text/markdown"
-    mock_file = io.BytesIO(MD_content)
+    mock_file = io.BytesIO(b"Idk")
 
     mock_tool = mocker.MagicMock()
     mock_tool.check.return_value = []
@@ -96,58 +104,48 @@ def test_check_endpoint_accepts_markdown_file(mocker):
     assert response.json() != error_responses["Invalid Markdown"]
 
 
-def test_save_endpoint_with_valid_input(tmp_path, monkeypatch: pytest.MonkeyPatch):
+def test_save_endpoint_with_valid_input(temp_markdown_file):
     # Replaces the top-level markdown_dir with tmp_path
-    monkeypatch.setattr("main.markdown_dir", tmp_path)
-
     MD_content_str = MD_content.decode()
     MD_content_hash = abs(hash(MD_content_str))
 
     response = client.post("/save", content=MD_content_str)
 
-    assert len(list(tmp_path.iterdir())) == 1
+    assert len(list(temp_markdown_file.iterdir())) == 1
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {"filename": MD_content_hash}
 
 
-def test_save_endpoint_with_empty_input(tmp_path, monkeypatch):
-    monkeypatch.setattr("main.markdown_dir", tmp_path)
+def test_save_endpoint_with_empty_input(temp_markdown_file):
     content = ""
-
     response = client.post("/save", content=content)
 
-    assert len(list(tmp_path.iterdir())) == 0
+    assert len(list(temp_markdown_file.iterdir())) == 0
     assert response.status_code == status.HTTP_422_UNPROCESSABLE_CONTENT
 
 
-def test_save_endpoint_with_non_utf8_characters(tmp_path, monkeypatch):
-    monkeypatch.setattr("main.markdown_dir", tmp_path)
-
+def test_save_endpoint_with_non_utf8_characters(temp_markdown_file):
     # Invalid utf-8 text
     content = b"\xff"
 
     response = client.post("/save", content=content)
 
-    assert len(list(tmp_path.iterdir())) == 0
+    assert len(list(temp_markdown_file.iterdir())) == 0
     assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == error_responses["Decode Error"]
 
 
-def test_notes_endpoint_with_no_saved_notes(tmp_path, monkeypatch):
-    monkeypatch.setattr("main.markdown_dir", tmp_path)
-
+def test_notes_endpoint_with_no_saved_notes(temp_markdown_file):
     response = client.get("/notes")
 
     assert response.json() == {"detail": "No notes saved yet..."}
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_notes_endpoint_with_saved_notes(tmp_path, monkeypatch):
-    monkeypatch.setattr("main.markdown_dir", tmp_path)
-
+def test_notes_endpoint_with_saved_notes(temp_markdown_file):
     # Create a fake file in the tmp_dir
     filename = "random.MD"
-    md = tmp_path / filename
+    md = temp_markdown_file / filename
     md.write_text("Random text")
 
     notes = {"name": filename}
